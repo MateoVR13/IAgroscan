bd65836ba029e807c95963bcb3c697cf052f85fb
MateoVR13 <mattvr130@gmail.com>
2024-05-17 10:27:05 -0500

    Detecciones Implementadas

--- src/gui/mainWindow.py
+++ src/gui/mainWindow.py
@@ -1,35 +1,38 @@
 import customtkinter
 from PIL import Image
 from simpleDetection import simpleDetection
+from tkinter import filedialog
+from ultralytics import YOLO
+import time, os
 
 class mainWindow(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         
-        #Window Setup
+       
         self.geometry("800x700+550+175")
         self.title("IAgroscan | Menú Principal")
         self.wm_iconbitmap("data/scanner.ico")
         customtkinter.set_appearance_mode("Dark")
         customtkinter.set_default_color_theme("green")
         
-        #Grid Configuration
+
         self.grid_columnconfigure((1), weight=1)
         self.grid_columnconfigure(2, weight=0)
         self.grid_rowconfigure((0, 1), weight=1)
         
-        #Sidebar Configuration
+
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=25)
         self.sidebar_frame.grid(row=0, column=0, padx=10, pady=10, rowspan=4, sticky="nswe")
         self.sidebar_frame.grid_rowconfigure(10, weight=1)
         
-        #Center Frame Configuration
+
         self.center_frame = customtkinter.CTkFrame(self, width=350, corner_radius=25)
         self.center_frame.grid(row=0, column=1, padx=10, pady=10, rowspan=4, sticky="nswe")
         self.center_frame.grid_rowconfigure(20, weight=1)
         self.center_frame.grid_columnconfigure(10, weight=1)
         
-        #Logo Setup
+
         logo = customtkinter.CTkImage(
             light_image=Image.open("data/iagroscan_logo.png"),
             dark_image=Image.open("data/iagroscan_logo.png"),
@@ -38,11 +41,11 @@ class mainWindow(customtkinter.CTk):
         self.labelImg = customtkinter.CTkLabel(self.sidebar_frame, image=logo, text="")
         self.labelImg.grid(row=1, column=0, padx=20, pady=(20, 10))
 
-        #Sidebar Components
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Detección Simple",fg_color="gray25")
+
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_image, text="Detección Simple",fg_color="gray25")
         self.sidebar_button_1.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
         
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Detección en Lote",fg_color="gray25")
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch, text="Detección en Lote",fg_color="gray25")
         self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
         
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Informes",fg_color="gray25")
@@ -51,14 +54,14 @@ class mainWindow(customtkinter.CTk):
         self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Estadísticas",fg_color="gray25")
         self.sidebar_button_4.grid(row=6, column=0, padx=20, pady=10, sticky="ew")
         
-        #Scaling Setup
+
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="Tamaño de la Interfaz:", anchor="w")
         self.scaling_label.grid(row=14, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event, fg_color="gray25")
         self.scaling_optionemenu.grid(row=15, column=0, padx=20, pady=(10, 20))
         
-        #Center Frame Components
+
         my_image = customtkinter.CTkImage(light_image=Image.open("data/defaultPicture.png"),
                                   dark_image=Image.open("data/defaultPicture.png"),
                                   size=(250, 250))
@@ -66,7 +69,7 @@ class mainWindow(customtkinter.CTk):
         self.detectedImage = customtkinter.CTkLabel(self.center_frame, image=my_image,width=250, height=250, text="", corner_radius=25)
         self.detectedImage.grid(row=4, column=10, padx=20, pady=(100, 10))
         
-        self.labelSuggestions = customtkinter.CTkLabel(self.center_frame, text="Recomendaciones", font=customtkinter.CTkFont(family="Google Sans Regular", size=17))
+        self.labelSuggestions = customtkinter.CTkLabel(self.center_frame, text="Recomendación", font=customtkinter.CTkFont(family="Google Sans Regular", size=17))
         self.labelSuggestions.grid(row=5, column=10, padx=20, pady=(10,0))
         
         self.labelTextSugg = customtkinter.CTkTextbox(self.center_frame, width=450,fg_color="transparent")
@@ -75,7 +78,7 @@ class mainWindow(customtkinter.CTk):
         self.labelTextSugg.tag_add("justificado", "1.0", "end")
         self.labelTextSugg.grid(row=6, column=10)
         
-        self.openDetection = customtkinter.CTkButton(self.center_frame, command=self.start_detection_command, width=120, text="Comenzar Detección Simple")
+        self.openDetection = customtkinter.CTkButton(self.center_frame, command=self.select_image, width=120, text="Comenzar Detección Simple")
         self.openDetection.grid(row=6, column=10)
 
 
@@ -95,8 +98,31 @@ class mainWindow(customtkinter.CTk):
         
         simple_detection_window = simpleDetection()
         simple_detection_window.mainloop()
-        self.destroy()
         
+    def select_image(self):
+    
+        file_path = filedialog.askopenfilename()
+        
+        model = YOLO('models/modelV3.pt')
+        results = model(file_path)
+
+
+        for result in results:
+            result.show() 
+            result.save(filename='result.jpg')
+        
+
+    def select_batch(self):
+        folder_path = filedialog.askdirectory()
+        
+        model = YOLO('models/modelV3.pt')
+        results = model(folder_path)
+
+        for i, result in enumerate(results):
+
+            timestamp = int(time.time() * 1000)
+            result_filename = os.path.join(folder_path, f'result_{timestamp}_{i}.jpg')
+            result.save(filename=result_filename)
 
         
 app = mainWindow()
--- src/gui/simpleDetection.py
+++ src/gui/simpleDetection.py
@@ -1,5 +1,11 @@
 import customtkinter
+import tkinter
+from tkinter import filedialog
 from PIL import Image
+from ultralytics import YOLO
+
+
+
 
 class simpleDetection(customtkinter.CTk):
     def __init__(self):
@@ -8,7 +14,7 @@ class simpleDetection(customtkinter.CTk):
         #Window Setup
         self.geometry("800x700+550+175")
         self.title("IAgroscan | Detección Simple")
-        self.wm_iconbitmap("data\scanner.ico")
+        self.wm_iconbitmap("data/scanner.ico")
         customtkinter.set_appearance_mode("Dark")
         customtkinter.set_default_color_theme("green")
         
@@ -23,10 +29,15 @@ class simpleDetection(customtkinter.CTk):
         self.top_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nswe")
         self.top_frame.grid_columnconfigure(5, weight=1)
         
+        self.upload_button = customtkinter.CTkButton(self.top_frame, command=self.select_image, text="Detección Simple",fg_color="gray25")
+        self.upload_button.grid(row=2, column=0, padx=20, pady=10, sticky="nswe")
+            
         #TopRightLeft Frame Configuration
         self.top_right_frame = customtkinter.CTkFrame(self, width=140, corner_radius=25)
         self.top_right_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nswe")
         self.top_right_frame.grid_columnconfigure(5, weight=1)
+        
+
         
         #BottomWhole Frame Configuration
         self.bottom_frame = customtkinter.CTkFrame(self, width=280, corner_radius=25)
