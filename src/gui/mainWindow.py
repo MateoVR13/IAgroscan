1a97a1606e3aae96ae53e24b72d38a5324437e14
MateoVR13 <mattvr130@gmail.com>
2024-05-19 10:17:57 -0500

    Re-making Interfaces

--- src/gui/mainWindow.py
+++ src/gui/mainWindow.py
@@ -28,77 +28,74 @@ class mainWindow(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         
-       
         self.geometry("800x700+550+175")
+        customtkinter.set_appearance_mode("light")
+        self.after(0, lambda:self.state('zoomed'))
         self.title("IAgroscan | Menú Principal")
         self.wm_iconbitmap("data/scanner.ico")
-        customtkinter.set_appearance_mode("Dark")
-        customtkinter.set_default_color_theme("green")
-        
 
+        customtkinter.set_default_color_theme("blue")
+        
         self.grid_columnconfigure((1), weight=1)
         self.grid_columnconfigure(2, weight=0)
         self.grid_rowconfigure((0, 1), weight=1)
         
-
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=25)
         self.sidebar_frame.grid(row=0, column=0, padx=10, pady=10, rowspan=4, sticky="nswe")
         self.sidebar_frame.grid_rowconfigure(10, weight=1)
         
-
         self.center_frame = customtkinter.CTkFrame(self, width=350, corner_radius=25)
         self.center_frame.grid(row=0, column=1, padx=10, pady=10, rowspan=4, sticky="nswe")
         self.center_frame.grid_rowconfigure(20, weight=1)
         self.center_frame.grid_columnconfigure(10, weight=1)
         
-
         logo = customtkinter.CTkImage(
             light_image=Image.open("data/iagroscan_logo.png"),
             dark_image=Image.open("data/iagroscan_logo.png"),
-            size=(200, 39),
+            size=(200, 45),
         )
+        
         self.labelImg = customtkinter.CTkLabel(self.sidebar_frame, image=logo, text="")
         self.labelImg.grid(row=1, column=0, padx=20, pady=(20, 10))
 
-
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_image, text="Detección Simple",fg_color="gray25")
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_image, text="Detección Simple")
         self.sidebar_button_1.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
         
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch, text="Detección en Lote",fg_color="gray25")
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch, text="Detección en Lote")
         self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
         
-        # self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Informes",fg_color="gray25")
-        # self.sidebar_button_3.grid(row=5, column=0, padx=20, pady=10, sticky="ew")
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Informes")
+        self.sidebar_button_3.grid(row=5, column=0, padx=20, pady=10, sticky="ew")
         
-        # self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Estadísticas",fg_color="gray25")
-        # self.sidebar_button_4.grid(row=6, column=0, padx=20, pady=10, sticky="ew")
-        
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Estadísticas")
+        self.sidebar_button_4.grid(row=6, column=0, padx=20, pady=10, sticky="ew")
 
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="Tamaño de la Interfaz:", anchor="w")
         self.scaling_label.grid(row=14, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event, fg_color="gray25")
+                                                               command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=15, column=0, padx=20, pady=(10, 20))
         
 
-        my_image = customtkinter.CTkImage(light_image=Image.open("data/defaultPicture.png"),
-                                  dark_image=Image.open("data/defaultPicture.png"),
-                                  size=(250, 250))
+        # my_image = customtkinter.CTkImage(light_image=Image.open("data/defaultPicture.png"),
+        #                           dark_image=Image.open("data/defaultPicture.png"),
+        #                           size=(250, 250))
         
-        self.detectedImage = customtkinter.CTkLabel(self.center_frame, image=my_image,width=250, height=250, text="", corner_radius=25)
-        self.detectedImage.grid(row=4, column=10, padx=20, pady=(100, 10))
+        # self.detectedImage = customtkinter.CTkLabel(self.center_frame, image=my_image,width=250, height=250, text="", corner_radius=25)
+        # self.detectedImage.grid(row=4, column=10, padx=20, pady=(100, 10))
         
-        self.labelSuggestions = customtkinter.CTkLabel(self.center_frame, text="Recomendación", font=customtkinter.CTkFont(family="Google Sans Medium", size=17))
-        self.labelSuggestions.grid(row=5, column=10, padx=20, pady=(10,0))
+        # self.labelSuggestions = customtkinter.CTkLabel(self.center_frame, text="Recomendación", font=customtkinter.CTkFont(family="Google Sans Medium", size=17))
+        # self.labelSuggestions.grid(row=5, column=10, padx=20, pady=(10,0))
         
-        self.labelTextSugg = customtkinter.CTkTextbox(self.center_frame, width=450,fg_color="transparent")
-        self.labelTextSugg.insert("0.0", """Para obtener mejores resultados es recomendable enfocar la enfermedad de la hoja en el centro de la imagen y usar una relación de aspecto 1:1.\n""")
-        self.labelTextSugg.tag_config("justificado", justify='center', wrap='word')
-        self.labelTextSugg.tag_add("justificado", "1.0", "end")
-        self.labelTextSugg.grid(row=6, column=10)
+        # self.labelTextSugg = customtkinter.CTkTextbox(self.center_frame, width=450,fg_color="transparent")
+        # self.labelTextSugg.insert("0.0", """Para obtener mejores resultados es recomendable enfocar la enfermedad de la hoja en el centro de la imagen y usar una relación de aspecto 1:1.\n""")
+        # self.labelTextSugg.tag_config("justificado", justify='center', wrap='word')
+        # self.labelTextSugg.tag_add("justificado", "1.0", "end")
+        # self.labelTextSugg.configure(state="disabled")
+        # self.labelTextSugg.grid(row=6, column=10)
         
-        self.openDetection = customtkinter.CTkButton(self.center_frame, command=self.select_image, width=120, text="Comenzar Detección Simple")
-        self.openDetection.grid(row=6, column=10)
+        # self.openDetection = customtkinter.CTkButton(self.center_frame, command=self.select_image, width=120, text="Comenzar Detección Simple")
+        # self.openDetection.grid(row=6, column=10)
 
         self.toplevel_window = None
 
@@ -129,7 +126,7 @@ class mainWindow(customtkinter.CTk):
     
         file_path = filedialog.askopenfilename()
         
-        model = YOLO('models/modelV2.pt')
+        model = YOLO('models/modelV3.pt')
         results = model(file_path)
 
 
@@ -153,9 +150,6 @@ class mainWindow(customtkinter.CTk):
         global rutaCarpeta
         rutaCarpeta = str(folder_path)    
         self.open_toplevel()
-
-        # dialog = customtkinter.CTkInputDialog(text="Las imágenes se han guardado en la ruta:", title="Detección el Lote Exitosa!")
-
-        
+      
 app = mainWindow()
 app.mainloop()
