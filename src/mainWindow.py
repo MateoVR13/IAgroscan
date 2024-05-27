3317b6f5d23870d42393a00784190610fcc42f2b
MateoVR13 <mattvr130@gmail.com>
2024-05-27 15:41:54 -0500

    Nueva UI con todos los menús.

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -41,42 +41,125 @@ class mainWindow(customtkinter.CTk):
         self.grid_columnconfigure(2, weight=0)
         self.grid_rowconfigure((0, 1), weight=1)
         
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=25)
-        self.sidebar_frame.grid(row=0, column=0, padx=10, pady=10, rowspan=4, sticky="nswe")
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, fg_color="sea green", corner_radius=25)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, padx=10, pady=10, sticky="nswe")
         self.sidebar_frame.grid_rowconfigure(10, weight=1)
         
         self.center_frame = customtkinter.CTkFrame(self, width=350, corner_radius=25)
-        self.center_frame.grid(row=0, column=1, padx=10, pady=10, rowspan=4, sticky="nswe")
+        self.center_frame.grid(row=0, column=1, padx=(0,10), pady=10, rowspan=4, sticky="nswe")
         self.center_frame.grid_rowconfigure(20, weight=1)
         self.center_frame.grid_columnconfigure(10, weight=1)
         
         logo = customtkinter.CTkImage(
-            light_image=Image.open("data/imgs/iagroscan_logo.png"),
-            dark_image=Image.open("data/imgs/iagroscan_logo.png"),
-            size=(200, 45),
+            light_image=Image.open("data/imgs/Scanner2.png"),
+            dark_image=Image.open("data/imgs/Scanner2.png"),
+            size=(200, 200),
         )
         
         self.labelImg = customtkinter.CTkLabel(self.sidebar_frame, image=logo, text="")
         self.labelImg.grid(row=1, column=0, padx=20, pady=(20, 10))
+        
+        img = Image.open("data/icons/scannerw.png")
+        img2 = Image.open("data/icons/scannerg.png")
+        
+        img3 = Image.open("data/icons/cellsw.png")
+        img4 = Image.open("data/icons/cellsg.png")
+        
+        img5 = Image.open("data/icons/bar_chartw.png")
+        img6 = Image.open("data/icons/bar_chartg.png")
+        
+        img7 = Image.open("data/icons/reportw.png")
+        img8 = Image.open("data/icons/reportg.png")
+        
 
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_image, text="Detección Simple", height=60, corner_radius=30, font=customtkinter.CTkFont(family="Google Sans Medium", size=18))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_image, 
+                                                        text="Detección Simple", fg_color="transparent",
+                                                        height=60, text_color="white",
+                                                        corner_radius=15, anchor="w",
+                                                        font=("Google Sans Medium", 18),
+                                                        image= customtkinter.CTkImage(dark_image=img, light_image=img))
+        
+
+        def on_hover(event):
+            self.sidebar_button_1.configure(text_color="sea green")
+            self.sidebar_button_1.configure(fg_color="white")
+            self.sidebar_button_1.configure(image= customtkinter.CTkImage(dark_image=img2, light_image=img2))
+            
+        def off_hover(event):
+            self.sidebar_button_1.configure(text_color="white")
+            self.sidebar_button_1.configure(fg_color="transparent")
+            self.sidebar_button_1.configure(image= customtkinter.CTkImage(dark_image=img, light_image=img))
+            
+
+        self.sidebar_button_1.bind("<Enter>", on_hover)
+        self.sidebar_button_1.bind("<Leave>", off_hover)
         self.sidebar_button_1.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
         
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch, text="Detección en Lote", height=60, corner_radius=30, font=customtkinter.CTkFont(family="Google Sans Medium", size=18))
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch,
+                                                        text="Detección en Lote", fg_color="transparent",
+                                                        height=60, text_color="white",
+                                                        corner_radius=15, anchor="w",
+                                                        font=("Google Sans Medium", 18),
+                                                        image= customtkinter.CTkImage(dark_image=img, light_image=img))
+        
+        def on_hover(event):
+            self.sidebar_button_2.configure(text_color="sea green")
+            self.sidebar_button_2.configure(fg_color="white")
+            self.sidebar_button_2.configure(image= customtkinter.CTkImage(dark_image=img2, light_image=img2))
+            
+        def off_hover(event):
+            self.sidebar_button_2.configure(text_color="white")
+            self.sidebar_button_2.configure(fg_color="transparent")
+            self.sidebar_button_2.configure(image= customtkinter.CTkImage(dark_image=img, light_image=img))
+            
+        self.sidebar_button_2.bind("<Enter>", on_hover)
+        self.sidebar_button_2.bind("<Leave>", off_hover)
         self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
-
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="Tamaño de la Interfaz:", anchor="w")
-        self.scaling_label.grid(row=14, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=15, column=0, padx=20, pady=(10, 20))
+        
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch,
+                                                        text="Registros", fg_color="transparent",
+                                                        height=60, text_color="white",
+                                                        corner_radius=15, anchor="w",
+                                                        font=("Google Sans Medium", 18),
+                                                        image= customtkinter.CTkImage(dark_image=img3, light_image=img3))
+        
+        def on_hover(event):
+            self.sidebar_button_3.configure(text_color="sea green")
+            self.sidebar_button_3.configure(fg_color="white")
+            self.sidebar_button_3.configure(image= customtkinter.CTkImage(dark_image=img4, light_image=img4))
+            
+        def off_hover(event):
+            self.sidebar_button_3.configure(text_color="white")
+            self.sidebar_button_3.configure(fg_color="transparent")
+            self.sidebar_button_3.configure(image= customtkinter.CTkImage(dark_image=img3, light_image=img3))
+            
+        self.sidebar_button_3.bind("<Enter>", on_hover)
+        self.sidebar_button_3.bind("<Leave>", off_hover)
+        self.sidebar_button_3.grid(row=4, column=0, padx=20, pady=10, sticky="ew")
+        
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.select_batch,
+                                                        text="Gráficos", fg_color="transparent",
+                                                        height=60, text_color="white",
+                                                        corner_radius=15, anchor="w",
+                                                        font=("Google Sans Medium", 18),
+                                                        image= customtkinter.CTkImage(dark_image=img5, light_image=img5))
+        
+        def on_hover(event):
+            self.sidebar_button_4.configure(text_color="sea green")
+            self.sidebar_button_4.configure(fg_color="white")
+            self.sidebar_button_4.configure(image= customtkinter.CTkImage(dark_image=img6, light_image=img6))
+            
+        def off_hover(event):
+            self.sidebar_button_4.configure(text_color="white")
+            self.sidebar_button_4.configure(fg_color="transparent")
+            self.sidebar_button_4.configure(image= customtkinter.CTkImage(dark_image=img5, light_image=img5))
+            
+        self.sidebar_button_4.bind("<Enter>", on_hover)
+        self.sidebar_button_4.bind("<Leave>", off_hover)
+        self.sidebar_button_4.grid(row=5, column=0, padx=20, pady=10, sticky="ew")
         
         self.toplevel_window = None
 
-    def change_scaling_event(self, new_scaling: str):
-        new_scaling_float = int(new_scaling.replace("%", "")) / 100
-        customtkinter.set_widget_scaling(new_scaling_float)
-        
     def open_toplevel(self):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
             self.toplevel_window = ToplevelWindow(self)
