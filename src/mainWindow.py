63aedeac2b1e61fa7c8a6def8c245e5ef6fe7461
Mateo Vergara <110215844+MateoVR13@users.noreply.github.com>
2024-08-11 09:33:17 -0500

    Update mainWindow.py

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -259,7 +259,7 @@ class mainWindow(customtkinter.CTk):
         c.drawString(50, height - 200, f"Efectividad de Detección -------------------------------------------------------------------------- {detection_effectiveness:.2f}%")
 
 
-        api_key = "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"
+        api_key = "REEMPLAZAR POR API KEY"
         client = OpenAI(api_key=api_key)
 
         diseases_detected = ", ".join(detected_classes.keys())
